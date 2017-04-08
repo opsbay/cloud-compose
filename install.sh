46040dc0ac37b7e1f8421d862d7e4292bc4f6dd0
Pierre-David Bélanger <pierredavidbelanger@gmail.com>
2017-04-08 18:57:27 -0400

    upgrade compose

--- install.sh
+++ install.sh
@@ -24,10 +24,11 @@ sudo apt-get update
 sudo apt-get install -y docker-ce
 sudo systemctl enable docker
 sudo service docker restart
+sudo gpasswd -a ubuntu docker
 
 # docker-compose
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.12.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # cloud-compose
